d54ffa92544e66a5b7a73fa545f0e3f28d371980
Bhavesh <bhavesh@jklu.edu.in>
2023-06-03 00:45:09 +0530

    seo improved

--- components/Head/index.js
+++ components/Head/index.js
@@ -8,20 +8,20 @@ export default function DomHead({ pageName = "Home Page" }) {
             <title>{userInfo.github_username} Portfolio - {pageName} </title>
             {/* meta tags begins */}
             {/* Primary Meta Tags */}
-            <meta name="title" content="Benrobo- Personal Portfolio." />
-            <meta name="description" content="Generated using BaayMax." />
+            <meta name="title" content="Bhavesh- Personal Portfolio." />
+            <meta name="description" content="Driven by a mission to create impactful solutions with a focus on user experience, teamwork, and knowledge sharing." />
             {/* Open Graph / Facebook */}
             <meta property="og:type" content="website" />
             <meta property="og:url" content="https://github.com/benrobo/portfolio-v3/" />
-            <meta property="og:title" content="Benrobo- Personal Portfolio" />
-            <meta property="og:description" content="Generating of portfolio just got easier. Using BaayMax and a single command, you're on set." />
+            <meta property="og:title" content="Bhavesh - Personal Portfolio" />
+            <meta property="og:description" content="Transforming complex problems into elegant solutions with the power of code." />
             <meta property="og:image" content="https://github.com/Benrobo/baaymax-assets/blob/main/app.png?raw=true" />
 
             {/* <!-- Twitter --> */}
             <meta property="twitter:card" content="summary_large_image" />
-            <meta property="twitter:url" content="https://metatags.io/" />
-            <meta property="twitter:title" content="Benrobo- Personal Portfolio" />
-            <meta property="twitter:description" content="Generating of portfolio just got easier. Using BaayMax and a single command, you're on set." />
+            <meta property="twitter:url" content="https://bhavesh.engineer/" />
+            <meta property="twitter:title" content="Bhavesh - Personal Portfolio" />
+            <meta property="twitter:description" content="Transforming complex problems into elegant solutions with the power of code." />
             <meta property="twitter:image" content="https://github.com/Benrobo/baaymax-assets/blob/main/app.png?raw=true" />
 
             {/* meta tags end */}
